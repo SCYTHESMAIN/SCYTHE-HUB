5a4a7459b4ef4107790ab3484455cd62723fa91d
Scythes <lxoudg@gmail.com>
2026-02-10 02:03:32 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,23 +1,12 @@
 return {
-	-- Murder Mystery 2
 	{
 		name = "Murder Mystery 2",
 		image = "rbxassetid://0",
 		script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/SCYTHESMAIN/SCYTHE-HUB-SCRIPTS/main/MM2_v1.0.lua"))()]]
 	},
-	
-	-- Operation Siege
 	{
 		name = "Operation Siege",
 		image = "rbxassetid://73335775092708",
 		script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/SCYTHESMAIN/SCYTHE-HUB-SCRIPTS/main/operationssiege_v1.1.lua"))()]]
 	},
-	
-	--[[
-	{
-		name = "Script Name",
-		image = "rbxassetid://0",
-		script = [[loadstring(game:HttpGet("SCRIPT_URL"))()]]
-	},
-	]]--
 }
